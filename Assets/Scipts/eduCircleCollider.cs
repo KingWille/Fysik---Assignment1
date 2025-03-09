11d9049c57106db486cd416ce12a3142851c1f33
William Friis <56981641+KingWille@users.noreply.github.com>
2025-03-09 22:32:43 +0100

    Initial setup of scripts

---
+++ Assets/Scipts/eduCircleCollider.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class eduCircleCollider : MonoBehaviour
+{
+    float m_radius;
+    float m_Density;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+}
---
+++ Assets/Scipts/eduRigidBody.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class eduRigidBody : MonoBehaviour
+{
+    Vector2 m_velocity;
+    float m_ang_velocity;
+    float m_force;
+    float m_torque;
+    float m_mass;
+    float m_inertia;
+    float m_restitution_coef;
+    int m_frame_skip;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        
+    }
+
+    private void ApplyForce(Vector2 f)
+    {
+
+    }
+
+    private void ApplyTorque(float t)
+    {
+
+    }
+
+    private void ApplyImpulse(Vector2 j)
+    {
+
+    }
+}
